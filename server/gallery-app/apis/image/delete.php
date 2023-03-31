b521ead613047722aa389cbf492d9642b8e44e19
DragonDev <thanhlongedu0304@gmail.com>
2023-03-31 14:57:38 +0700

    Gallery app

---
+++ server/gallery-app/apis/image/delete.php
@@ -0,0 +1,90 @@
+<?php
+//? ====================
+//? IMPORTS
+//? ====================
+require("../../core/config.php");
+require("../../core/connectDatabase.php");
+require("../../classes/ResponseAPI.php");
+require("../../helpers/functions.php");
+
+
+//? ====================
+//? HEADERS
+//? ====================
+header("Access-Control-Allow-Origin: " . ACCESS_CONTROL_ALLOW_ORIGIN);
+header("Access-Control-Allow-Methods: DELETE");
+header("Content-Type: application/json");
+
+
+//? ====================
+//? PARAMETERS & PAYLOAD
+//? ====================
+$tableName = "image";
+$data = getJSONPayloadRequest();
+
+$id = $data["id"] ?? ""; // int
+
+
+//? ====================
+//? START
+//? ====================
+// ✅ Xóa item theo id
+deleteItem($id);
+
+
+//? ====================
+//? FUNCTIONS
+//? ====================
+function deleteItem($id)
+{
+   global $connect, $tableName;
+
+   // Kiểm tra dữ liệu payload
+   if ($id === "" || !is_numeric($id)) {
+      $response = new ResponseAPI(9, "Không đủ payload để thực hiện");
+      $response->send();
+      return;
+   }
+
+   // Kiểm tra xem image có tồn tại trong DB không
+   $query = "SELECT * FROM `$tableName` WHERE `id` = '$id' LIMIT 1";
+   $result = mysqli_query($connect, $query);
+   $obj = null;
+
+   if (!$result || ($obj = $result->fetch_object()) === null) {
+      $response = new ResponseAPI(3, "Không tìm thấy file ảnh");
+      $response->send();
+      return;
+   }
+
+   // Lấy path file ảnh trên server
+   $fileLocation = LOCATION_UPLOAD_IMAGE . $obj->filename;
+
+   // Xóa file trên server
+   if (unlink($fileLocation)) {
+      // Thực thi query
+      $query = "DELETE FROM `$tableName` WHERE `id` = '$id'";
+      performsQueryAndResponseToClient($query);
+   } else {
+      $response = new ResponseAPI(2, "Xóa file ảnh thất bại");
+      $response->send();
+   }
+
+   // Đóng kết nối
+   $connect->close();
+}
+
+// Thực thi truy vấn và trả về kết quả cho Client
+function performsQueryAndResponseToClient($query)
+{
+   global $connect;
+   $result = mysqli_query($connect, $query);
+
+   if ($result) {
+      $response = new ResponseAPI(1, "Thành công");
+      $response->send();
+   } else {
+      $response = new ResponseAPI(2, "Thất bại");
+      $response->send();
+   }
+}
---
+++ server/gallery-app/apis/image/upload.php
@@ -0,0 +1,108 @@
+<?php
+//? ====================
+//? IMPORTS
+//? ====================
+require("../../core/config.php");
+require("../../core/connectDatabase.php");
+require("../../classes/ResponseAPI.php");
+require("../../helpers/functions.php");
+
+
+//? ====================
+//? HEADERS
+//? ====================
+header("Access-Control-Allow-Origin: " . ACCESS_CONTROL_ALLOW_ORIGIN);
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json");
+
+
+//? ====================
+//? PARAMETERS & PAYLOAD
+//? ====================
+$tableName = "image";
+
+$file = $_FILES['image'] ?? null; // file
+
+
+//? ====================
+//? START
+//? ====================
+// ✅ Tải ảnh lên server
+uploadImage($file);
+
+
+//? ====================
+//? FUNCTIONS
+//? ====================
+function uploadImage($file)
+{
+   global $connect, $tableName;
+
+   // Kiểm tra dữ liệu payload
+   if ($file === null) {
+      $response = new ResponseAPI(9, "Không đủ payload để thực hiện");
+      $response->send();
+      return;
+   }
+
+   // Thông tin file hình ảnh
+   $filename = $file['name'];
+   $fileTmpName = $file['tmp_name'];
+   $size = $file['size'];
+
+   // Lấy file extension
+   $imageFileType = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+   /* Kiểm tra định dạng file */
+   if (in_array($imageFileType, IMAGE_EXTENSIONS_VALIDATE)) {
+      // Tạo filename
+      while (true) {
+         $filename = uniqid('project_image', false) . ".$imageFileType";
+         if (!file_exists(LOCATION_UPLOAD_IMAGE . $filename)) break;
+      }
+
+      // Tạo link
+      $link = PATH_UPLOAD_IMAGE . $filename;
+
+      /* Upload file */
+      if (move_uploaded_file($fileTmpName, LOCATION_UPLOAD_IMAGE . $filename)) {
+
+         // Thực thi query
+         $query = "INSERT INTO `$tableName`(`link`, `filename`, `size`) VALUES('$link', '$filename', $size)";
+         performsQueryAndResponseToClient($query, $link, $filename, $size);
+         
+      } else {
+         $response = new ResponseAPI(2, "Tạo file ảnh thất bại");
+         $response->send();
+      }
+   } else {
+      $response = new ResponseAPI(3, "Chỉ chấp nhận các định dạng file sau: " . implode(" | ", IMAGE_EXTENSIONS_VALIDATE));
+      $response->send();
+   }
+
+   // Đóng kết nối
+   $connect->close();
+}
+
+// Thực thi truy vấn và trả về kết quả cho Client
+function performsQueryAndResponseToClient($query, $link, $filename, $size)
+{
+   global $connect;
+
+   $result = mysqli_query($connect, $query);
+
+   if ($result) {
+      $imageObj = new stdClass;
+
+      $imageObj->id = mysqli_insert_id($connect);
+      $imageObj->link = $link;
+      $imageObj->filename = $filename;
+      $imageObj->size = $size;
+
+      $response = new ResponseAPI(1, "Thành công", $imageObj, 1);
+      $response->send();
+   } else {
+      $response = new ResponseAPI(2, "Thất bại");
+      $response->send();
+   }
+}
